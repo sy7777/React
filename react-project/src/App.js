f04ee5e04ee7b7c441154042c69c53e0c970f8e0
yuqi <piecesy7@gmail.com>
2023-07-11 15:47:20 +0800

    context

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,91 +1,95 @@
-import classNames from 'classnames'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { createContext } from 'react'
 import './App.scss'
 
-// 兄弟组件通讯
+// 跨组件通讯 - Context
+// 1. 创建 Context 对象
+const ThemeContext = createContext()
 
-// 左侧 好友列表组件
-const Friends = ({ friends, onSelect, chatFriend }) => {
+// 2. 划定范围，提供共享数据
+// 3. 范围内的组件，获取共享数据
+
+// -------------------------侧边栏-----------------------
+const Sidebar = () => {
   return (
-    <div className="friends">
-      {friends.map(item => {
-        return (
-          <div
-            key={item.id}
-            className={classNames(
-              'friend',
-              item.id === chatFriend.id && 'selected'
-            )}
-            onClick={() => onSelect(item)}
-          >
-            <img src={item.avatar} className="avatar" alt="" />
-            <div className="info">
-              <div className="row">
-                <div className="name">{item.name}</div>
-                <div className="date">{item.dateStr}</div>
-              </div>
-              <div className="msg">{item.message}</div>
-            </div>
-          </div>
-        )
-      })}
+    <div className="sidebar">
+      <Menu />
+    </div>
+  )
+}
+const Menu = () => {
+  return (
+    <div className="menu">
+      <ul>
+        <MenuItem />
+        <MenuItem />
+      </ul>
+    </div>
+  )
+}
+const MenuItem = () => {
+  // 3. 范围内的组件，获取共享数据
+  const { theme } = useContext(ThemeContext)
+  return <li style={{ color: theme }}>菜单</li>
+}
+
+// -------------------------右侧内容-----------------------
+const Content = () => {
+  const { theme } = useContext(ThemeContext)
+  return (
+    <div className="content">
+      <div className="main" style={{ color: theme }}>
+        Context 跨组件通讯
+      </div>
+      <Footer />
+    </div>
+  )
+}
+const Footer = () => {
+  const { onReset } = useContext(ThemeContext)
+
+  return (
+    <div className="footer">
+      <button onClick={onReset}>重置主题</button>
     </div>
   )
 }
 
-// 右侧 聊天窗口组件
-const Chat = ({ friend }) => {
-  return (
-    <div className="chat-wrapper">
-      <div className="header">{friend.name}</div>
-      <div className="list"></div>
-      <div className="input"></div>
-    </div>
-  )
-}
-
-// 好友列表数据
-const defaultFriends = [
-  {
-    id: '13258165',
-    name: '周杰伦',
-    avatar:
-      'https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/comment/zhoujielun.jpeg',
-    dateStr: '刚刚',
-    message: '哎呦，不错哦',
-  },
-  {
-    id: '36080105',
-    name: '许嵩',
-    avatar:
-      'https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/comment/xusong.jpeg',
-    dateStr: '01/05',
-    message: '[语音]',
-  },
-]
 // 父组件
 const App = () => {
-  const [friends,] = useState(defaultFriends)
+  // 要共享的主题颜色
+  const [theme, setTheme] = useState('#1677FF')
 
-  // 1. 找到父组件，提供要共享的数据
-  const [chatFriend, setChatFriend] = useState(friends[0])
-
-  const onSelectFriend = friend => {
-    setChatFriend(friend)
+  // 重置主题的函数
+  const onReset = () => {
+    setTheme('#1677FF')
   }
 
   return (
     <div className="app">
-      {/* 好友列表 */}
-      {/* 3. 通过子到父通讯，来修改选中的好友 */}
-      <Friends
-        friends={friends}
-        onSelect={onSelectFriend}
-        chatFriend={chatFriend}
-      />
-      {/* 聊天窗口 */}
-      {/* 2. 通过父到子通讯，来展示好友名称 */}
-      <Chat friend={chatFriend} />
+      {/* 2. 划定范围，提供共享数据 */}
+      <ThemeContext.Provider
+        value={{
+          theme,
+          onReset,
+          // ...
+        }}
+      >
+        {/* 默认颜色： #1677FF */}
+        <input
+          className="theme-selector"
+          type="color"
+          value={theme}
+          onChange={e => setTheme(e.target.value)}
+        />
+
+        <div className="main">
+          {/* 侧边栏 */}
+          <Sidebar />
+          {/* 右侧内容 */}
+          <Content />
+        </div>
+      </ThemeContext.Provider>
     </div>
   )
 }
